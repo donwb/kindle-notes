7571878f53333d2470306b5f139676c86f08480f
Don Browning <don.browning@gmail.com>
2024-11-01 14:05:56 -0400

    added other stuff

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-mongoose.connect("mongodb+srv://osdev:MYziS2Br8l6wxdLn@cluster0.qmw1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.MONGODB_URI!)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
